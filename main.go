b951977e1ad8e366e0a7f3d8abf2977f4485adc5
Thomas Mickley-Doyle <tmickleydoyle@github.com>
2020-01-09 17:53:46 -0600

    update query input file

--- main.go
+++ main.go
@@ -1,35 +1,88 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"database/sql"
-	"fmt"
+	"encoding/json"
 	"log"
 	"os"
+	"reflect"
 	"time"
 
 	_ "github.com/prestodb/presto-go-client/presto"
 )
 
 var (
-	userName = os.Getenv("PRESTO_USERNAME")
+	userName   = os.Getenv("PRESTO_USERNAME")
 	prestoHost = os.Getenv("PRESTO_HOST")
 	prestoPort = os.Getenv("PRESTO_PORT")
+
+	cancelTime = 20 * time.Minute
+
+	file = "query.sql"
 )
 
-func main() {	
+func main() {
+	filerc, err := os.Open(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer filerc.Close()
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(filerc)
+	f := buf.String()
+
 	dsn := "http://" + userName + "@" + prestoHost + ":" + prestoPort
 	db, err := sql.Open("presto", dsn)
-	ctx, _ := context.WithTimeout(context.Background(), 20*time.Minute)
 
-	if err == nil {
-		rows, err := db.QueryContext(ctx, "SELECT * FROM tables LIMIT 10")
-		if err != nil {
-			log.Fatal(err)
-		}
-	
-		for rows.Next() {
-			fmt.Println(rows)
-		}
+	b, err := queryToJson(db, f)
+	if err != nil {
+		log.Fatalln(err)
 	}
+	os.Stdout.Write(b)
+
+}
+
+func queryToJson(db *sql.DB, query string) ([]byte, error) {
+	// an array of JSON objects
+	// the map key is the field name
+	var objects []map[string]interface{}
+
+	ctx, cancel := context.WithTimeout(context.Background(), cancelTime)
+
+	rows, err := db.QueryContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	defer cancel()
+
+	for rows.Next() {
+		// figure out what columns were returned
+		// the column names will be the JSON object field keys
+		columns, err := rows.ColumnTypes()
+		if err != nil {
+			return nil, err
+		}
+
+		// Scan needs an array of pointers to the values it is setting
+		// This creates the object and sets the values correctly
+		values := make([]interface{}, len(columns))
+		object := map[string]interface{}{}
+		for i, column := range columns {
+			object[column.Name()] = reflect.New(column.ScanType()).Interface()
+			values[i] = object[column.Name()]
+		}
+
+		err = rows.Scan(values...)
+		if err != nil {
+			return nil, err
+		}
+
+		objects = append(objects, object)
+	}
+
+	// indent because I want to read the output
+	return json.MarshalIndent(objects, "", "\t")
 }
---
+++ query.sql
@@ -0,0 +1,6 @@
+SELECT
+    *
+FROM
+    users
+LIMIT
+    2
