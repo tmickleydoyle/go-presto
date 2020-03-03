5ee778cde8d0a7b1d2ad5dabc95be1c6c4c35352
Thomas Mickley-Doyle <tmickleydoyle@github.com>
2020-03-03 17:54:28 -0600

    update logic for CLI

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
-module main.go
+module github.com/tmickleydoyle/go-presto
 
 go 1.13
+
+require (
+	github.com/go-openapi/strfmt v0.19.4 // indirect
+	github.com/jedib0t/go-pretty v4.3.0+incompatible
+	github.com/joho/sqltocsv v0.0.0-20190824231449-5650f27fd5b6
+	github.com/mattn/go-runewidth v0.0.8 // indirect
+	github.com/prestodb/presto-go-client v0.0.0-20200302111820-5ec09431be26
+)
--- main.go
+++ main.go
@@ -12,9 +12,11 @@ import (
 	"log"
 	"os"
 	"reflect"
+	"strings"
 	"time"
 
 	"github.com/joho/sqltocsv"
+	"github.com/jedib0t/go-pretty/table"
 	_ "github.com/prestodb/presto-go-client/presto"
 )
 
@@ -26,8 +28,8 @@ var (
 	cancelTime = 1440 * time.Minute
 
 	jsonBool bool
-	in   string
-	out  string
+	in       string
+	out      string
 )
 
 const (
@@ -47,19 +49,21 @@ func main() {
 }
 
 func run(jsonOutput bool, filename string, outFilename string) error {
-	if filename == "" {
-		return errors.New("no input SQL file")
-	}
+	var f string
 
-	filerc, err := os.Open(filename)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer filerc.Close()
+	if _, err := os.Stat(filename); err == nil {
+		filerc, err := os.Open(filename)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer filerc.Close()
 
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(filerc)
-	f := buf.String()
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(filerc)
+		f = buf.String()
+	} else {
+		f = filename
+	}
 
 	dsn := "http://" + userName + "@" + prestoHost + ":" + prestoPort
 	db, err := sql.Open("presto", dsn)
@@ -76,25 +80,86 @@ func run(jsonOutput bool, filename string, outFilename string) error {
 	db.Close()
 
 	if err != nil {
-		return errors.New("error running query")
+		return errors.New(err.Error())
 	}
 
-	if outFilename != "" {
-		if jsonOutput == true {
-			b, err := queryToJson(rows)
-			if err != nil {
-				log.Fatalln(err)
-			}
+	// if outFilename != "" {
+	if jsonOutput == true {
+		b, err := queryToJson(rows)
+		if err != nil {
+			log.Fatalln(err)
+		}
 
-			jData := string(b)
+		jData := string(b)
+		
+		if outFilename != "" {
 			file, _ := json.MarshalIndent(jData, "", " ")
 			_ = ioutil.WriteFile(outFilename, file, 0644)
 		} else {
-			csvConverter := sqltocsv.New(rows)
+			fmt.Println(jData)
+		}
+	} else {
+		csvConverter := sqltocsv.New(rows)
+		if outFilename != "" {
 			csvConverter.WriteFile(outFilename)
+		} else {
+			t := table.NewWriter()
+			t.SetOutputMirror(os.Stdout)
+
+			columnNames, _ := rows.Columns()
+			count := len(columnNames)
+			values := make([]interface{}, count)
+			valuePtrs := make([]interface{}, count)
+			colNames := make([]interface{}, count)
+
+			for i, col := range columnNames {
+				colNames[i] = fmt.Sprintf("%v", col)
+				}
+			
+			t.AppendHeader(colNames)
+
+			for rows.Next() {
+				row := make([]interface{}, count)
+		
+				for i, _ := range columnNames {
+					valuePtrs[i] = &values[i]
+				}
+		
+				if err = rows.Scan(valuePtrs...); err != nil {
+					return err
+				}
+		
+				for i, _ := range columnNames {
+					var value interface{}
+					rawValue := values[i]
+		
+					byteArray, ok := rawValue.([]byte)
+					if ok {
+						value = string(byteArray)
+					} else {
+						value = rawValue
+					}
+		
+					timeValue, ok := value.(time.Time)
+					if ok && csvConverter.TimeFormat != "" {
+						value = timeValue.Format(csvConverter.TimeFormat)
+					}
+		
+					if value == nil {
+						row[i] = ""
+					} else {
+						cleanValue := fmt.Sprintf("%v", value)
+						row[i] = strings.Replace(cleanValue, " ", "_", -1)
+					}
+
+					
+				}
+				t.AppendRow(row)
+				fmt.Println(row)
+			}
+			t.Render()
 		}
 	}
-
 	return nil
 }
 
